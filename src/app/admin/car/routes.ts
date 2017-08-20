3aca04e60d2674eb94a7517154303eaa081f716f
mohmmedwee <eloustah@gmail.com>
2017-08-20 15:21:19 +0300

    import *

--- src/app/admin/car/routes.ts
+++ src/app/admin/car/routes.ts
@@ -1,16 +1,10 @@
-import { Create } from '../car/indexes';
-import { Index } from '../car/indexes';
-import { Edit } from '../car/indexes';
-import { Show } from '../car/indexes';
-
-
-
+import * as Car from '../car/indexes';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './../../../common/auth.guard';
 
 export const SECURE_ROUTES_MODEL: Routes = [
-    {path:"admin/car/create", component:Create},
-    {path:"admin/car", component:Index , canActivate: [AuthGuard] },
-    {path:"admin/car/:id/edit", component:Edit},
-    {path:"admin/car/:id", component:Show},
+    {path:"admin/car/create", component:Car.Create},
+    {path:"admin/car", component:Car.Index , canActivate: [AuthGuard] },
+    {path:"admin/car/:id/edit", component:Car.Edit},
+    {path:"admin/car/:id", component:Car.Show},
   ];
