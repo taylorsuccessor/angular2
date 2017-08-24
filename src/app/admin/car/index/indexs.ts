d066e1817a8c2a4f368d7a14774f1bdfb8413c5d
mohmmedwee <eloustah@gmail.com>
2017-08-24 09:25:11 +0300

    print

--- src/app/admin/car/index/indexs.ts
+++ src/app/admin/car/index/indexs.ts
@@ -105,7 +105,22 @@ SortByNum(n1: Model, n2: Model) {
         this.models = this.allItems.slice(this.pager.startIndex, this.pager.endIndex + 1);
     }
 
-
-
+z(){
+  return 332;
 }
+
+  
+   printToCart(printSectionId: string){
+        let popupWinindow
+        let innerContents = document.getElementById(printSectionId).innerHTML;
+        popupWinindow = window.open('', '_blank', 'width=600,height=700,scrollbars=no,menubar=no,toolbar=no,location=no,status=no,titlebar=no');
+        popupWinindow.document.open();
+        popupWinindow.document.write('<html><head><link rel="stylesheet" type="text/css" href="style.css" /></head><body onload="window.print()">' + innerContents + '</html>');
+        popupWinindow.document.close();
+  }
+  
+  
+  
+  }
+
 
